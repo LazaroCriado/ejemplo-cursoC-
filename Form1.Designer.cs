824ec993907d2f0d0ee633340cef95dfa68c64e6
lcriado <lcriado@mbmed.com>
2024-10-26 19:29:19 -0300

    primer commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace ejemplo1
+{
+
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.opTernarioCheckBox = new System.Windows.Forms.CheckBox();
+            this.opTernarioLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // opTernarioCheckBox
+            // 
+            this.opTernarioCheckBox.AutoSize = true;
+            this.opTernarioCheckBox.Location = new System.Drawing.Point(298, 185);
+            this.opTernarioCheckBox.Name = "opTernarioCheckBox";
+            this.opTernarioCheckBox.Size = new System.Drawing.Size(164, 24);
+            this.opTernarioCheckBox.TabIndex = 1;
+            this.opTernarioCheckBox.Text = "Operador Ternario";
+            this.opTernarioCheckBox.UseVisualStyleBackColor = true;
+            this.opTernarioCheckBox.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // opTernarioLabel
+            // 
+            this.opTernarioLabel.AutoSize = true;
+            this.opTernarioLabel.Location = new System.Drawing.Point(347, 244);
+            this.opTernarioLabel.Name = "opTernarioLabel";
+            this.opTernarioLabel.Size = new System.Drawing.Size(0, 20);
+            this.opTernarioLabel.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.opTernarioLabel);
+            this.Controls.Add(this.opTernarioCheckBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox opTernarioCheckBox;
+        private System.Windows.Forms.Label opTernarioLabel;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ejemplo1
+{
+    //una clase PARTIAL es una clase que se puede "partir" en disintos pedazos de codigo.
+    //es decir que puede tener un par de cosas definidas aca y el resto en otro archivo con un nombre de clase IGUAL
+    // al ponerle el partial adelante, lo que digo es que es la misma clase solo que dividida.
+    // aca es util porque en el Form1.Designer.cs tengo el pedazo de cogido de la clase Form1 que tiene el diseño
+    // de los objetos que forman la app.
+    // y en este archivo (Form1.cs) voy a poder codear la funcionalidad de dichos objetos. 
+    // Todo es parte de la clase Form1, pero dividio queda mas organizado.
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            string opTernario = opTernarioCheckBox.Checked == true ? "Verdadero" : "Falso" ;
+            opTernarioLabel.Text = opTernario;
+        }
+    }
+}
